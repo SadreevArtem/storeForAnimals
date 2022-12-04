e7ce6a35ae9d0e7bc08df327a5646a693017291d
Artem Sadreev <a.sadreew@yandex.ru>
2022-12-04 15:47:16 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import stylesHeader from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={stylesHeader.header}>
+      Header
+    </header>
+  )
+}
